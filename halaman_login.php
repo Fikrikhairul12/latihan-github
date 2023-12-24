97c0794a4dcc5a5a972c48d5768bfbb6e1972e4b
Fikrikhairul12 <khairulfikri1289@gmail.com>
2023-12-25 00:24:38 +0700

    add

---
+++ halaman_login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div class="login-container">
+    <h2>Masuk</h2>
+    <form action="#" method="POST">
+        <div class="input-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" required>
+        </div>
+        <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <button type="submit" class="btn">Masuk</button>
+    </form>
+</div>
+
+</body>
+</html>
