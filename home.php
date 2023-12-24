21161c5fe2f1f56571cd56cb596f1915cf754593
Fikrikhairul12 <khairulfikri1289@gmail.com>
2023-12-25 00:19:17 +0700

    latihan github

---
+++ home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Website Responsive</title>
+    <meta name="viewport" content="width=device-width, initiak-scale=1.o">
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1 align="center"> Talk To Me </h1>
+        </div>
+        <div class="main">
+            <div class="left">
+                <h3 align="center">Menu</h3>
+                <ul>
+                    <li><a href="halaman_login.php">Login</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Statistik</a></li>
+                </ul>
+            </div>
+            <div class="middle">
+                <h3 align="center">Artikel Ahli</h3>
+                <p align="center">Apa Tanda-tanda dari Toxic Parenting</p>
+                <img src="image/mental.jfif" alt="foto">
+                <p><a href="#">Baca Selengkapnya >></a></p>
+            </div>
+            <div class="right">
+                <h3 align="center">Sumber Perawatan</h3>
+                <p>
+                    <ul>
+                        <li><a href="#">Konsultasi Daring</a></li>
+                        <li><a href="#">Latihan Terpadu</a></li>
+                        <li><a href="#">Tes Psikologi</a></li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+        <div class="footer">
+            <p align="center">Copyright &copy; 2023 Responsive Konsultasi Mental</p>
+        </div>
+    </div>
+</body>
+</html>
